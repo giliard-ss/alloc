d6b694c3886e61582830a2b8baab81b037294059
Giliard Sousa <giliard_sousa@hotmail.com>
2021-01-11 19:04:16 -0300

    test

--- lib/main.dart
+++ lib/main.dart
@@ -5,13 +5,13 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.ggss
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.gil
+        // This is the theme of your application.
         //
         // Try running your application with "flutter run". You'll see the
         // application has a blue toolbar. Then, without quitting the app, try
